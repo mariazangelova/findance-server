d400cf9b0ae29cc6a8d998efb4fccd7f2a4494c0
mariazangelova <mariazangelova@gmail.com>
2020-05-24 21:27:51 +0200

    Routers for get, post and delete dancers

--- routers/dancers.js
+++ routers/dancers.js
@@ -10,14 +10,42 @@ router.get("/", async (req, res, next) => {
   res.status(200).send(dancers);
 });
 
-router.post("/:id", async (req, res, next) => {
-  console.log("REQUEST", req.body);
+router.get("/:id", async (req, res, next) => {
   const eventId = req.params.id;
   console.log("eventid", eventId);
-  const userId = 1;
+  const dancers = await Dancer.findAll({
+    include: [User],
+    where: { eventId },
+  });
+  res.status(200).send(dancers);
+});
+
+router.post("/:id", async (req, res, next) => {
+  const eventId = req.params.id;
+  const userId = req.body.userId;
   try {
-    Dancer.create({ eventId, userId });
-    return res.status(201).send({ message: "Dancer added" });
+    await Dancer.create({ eventId, userId });
+    const dancers = await Dancer.findAll({
+      include: [User],
+      where: { eventId },
+    });
+    return res.status(201).send({ message: "Dancer added", data: dancers });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/delete/:id", async (req, res, next) => {
+  const eventId = req.params.id;
+  const userId = req.body.userId;
+  console.log("USER ID", req.body);
+  try {
+    await Dancer.destroy({ where: { eventId, userId } });
+    const dancers = await Dancer.findAll({
+      include: [User],
+      where: { eventId },
+    });
+    return res.status(202).send({ message: "Dancer removed", data: dancers });
   } catch (e) {
     next(e);
   }
